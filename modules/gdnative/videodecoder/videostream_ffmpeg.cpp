8396b0a50c5746c0528d1cf78a4f5e1eb4965088
Anish <anishbhobe@hotmail.com>
2018-05-25 18:59:24 +0530

    Added FFMPEG temporarily for making the videodecoder.
    Working on internals right now.

---
+++ modules/gdnative/videodecoder/videostream_ffmpeg.cpp
@@ -0,0 +1,44 @@
+/*************************************************************************/
+/*  video_stream_webm.cpp                                                */
+/*************************************************************************/
+/*                       This file is part of:                           */
+/*                           GODOT ENGINE                                */
+/*                      https://godotengine.org                          */
+/*************************************************************************/
+/* Copyright (c) 2007-2018 Juan Linietsky, Ariel Manzur.                 */
+/* Copyright (c) 2014-2018 Godot Engine contributors (cf. AUTHORS.md)    */
+/*                                                                       */
+/* Permission is hereby granted, free of charge, to any person obtaining */
+/* a copy of this software and associated documentation files (the       */
+/* "Software"), to deal in the Software without restriction, including   */
+/* without limitation the rights to use, copy, modify, merge, publish,   */
+/* distribute, sublicense, and/or sell copies of the Software, and to    */
+/* permit persons to whom the Software is furnished to do so, subject to */
+/* the following conditions:                                             */
+/*                                                                       */
+/* The above copyright notice and this permission notice shall be        */
+/* included in all copies or substantial portions of the Software.       */
+/*                                                                       */
+/* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,       */
+/* EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF    */
+/* MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.*/
+/* IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY  */
+/* CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,  */
+/* TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE     */
+/* SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                */
+/*************************************************************************/
+
+
+#include "modules/gdnative/videodecoder/ffmpeg/include/libavformat/avformat.h"
+
+VideoStreamPlayback::open_file(const String& p_file) {
+    file_name = p_file;
+
+    AVFormatContext* pFormatCtx = nullptr;
+    if (avformat_open_input(&pFormatCtx, file_name.utf8().get_data(), nullptr, nullptr) != 0) {
+        
+    }
+    if (avformat_find_stream_info(pFormatCtx, nullptr) < 0) {
+        return -1;
+    }
+}
