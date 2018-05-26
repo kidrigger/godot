acbdf71a38e8933e614c3170435877eb9081f84a
Anish <anishbhobe@hotmail.com>
2018-05-26 22:30:38 +0530

    Added file reading and opening by FFMPEG.
    So far, FFMPEG can open and read files.
    Basic setup of videostream.

--- modules/gdnative/videodecoder/videostream_ffmpeg.cpp
+++ modules/gdnative/videodecoder/videostream_ffmpeg.cpp
@@ -28,17 +28,131 @@
 /* SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                */
 /*************************************************************************/
 
+#include "videostream_ffmpeg.h"
 
-#include "modules/gdnative/videodecoder/ffmpeg/include/libavformat/avformat.h"
+int ffmpeg_read_packet(void *ptr, uint8_t *buf, int buf_size) {
+	// ptr is a FileAccess
+	FileAccess *file = reinterpret_cast<FileAccess *>(ptr);
 
-VideoStreamPlayback::open_file(const String& p_file) {
-    file_name = p_file;
+	// if file exists
+	if (file) {
+		long bytes_read = file->get_buffer(buf, buf_size);
+		// No bytes to read => EOF
+		if (bytes_read == 0) {
+			return AVERROR_EOF;
+		}
+		return bytes_read;
+	}
+	return -1;
+}
 
-    AVFormatContext* pFormatCtx = nullptr;
-    if (avformat_open_input(&pFormatCtx, file_name.utf8().get_data(), nullptr, nullptr) != 0) {
-        
-    }
-    if (avformat_find_stream_info(pFormatCtx, nullptr) < 0) {
-        return -1;
-    }
+int64_t ffmpeg_seek_packet(void *ptr, int64_t pos, int whence) {
+	// file
+	FileAccess *file = reinterpret_cast<FileAccess *>(ptr);
+
+	size_t len = file->get_len();
+	if (file) {
+		switch (whence) {
+			case SEEK_SET: {
+				// Just for explicitness
+				size_t new_pos = static_cast<size_t>(pos);
+				if (new_pos > len) {
+					return -1;
+				}
+				file->seek(new_pos);
+				pos = static_cast<int64_t>(file->get_position());
+				return pos;
+			} break;
+			case SEEK_END: {
+				// Just in case something goes wrong
+				if (-pos > len) {
+					return -1;
+				}
+				file->seek_end(pos);
+				pos = static_cast<int64_t>(file->get_position());
+				return pos;
+			} break;
+			case SEEK_CUR: {
+				// Just in case it doesn't exist
+				if (pos < 0 && -pos > file->get_position()) {
+					return -1;
+				}
+				pos = pos + static_cast<int>(file->get_position());
+				file->seek(pos);
+				pos = static_cast<int64_t>(file->get_position());
+				return pos;
+			} break;
+			case AVSEEK_SIZE: {
+				// Asks to return the length of file
+				return static_cast<int64_t>(len);
+			} break;
+		}
+	}
+	// In case nothing works out.
+	return -1;
+}
+
+bool VideoStreamPlaybackFFMPEG::open_file(const String &p_file) {
+
+	// TODO Remove all the unnecessary comments.
+	// NOTE Only meant to open file once.
+
+	// If playing just err out and don't do anything
+	if (playing) {
+		return false;
+	}
+
+	// set the file name
+	file_name = p_file;
+
+	// delete file if exists
+	if (file) {
+		memdelete(file);
+	}
+	// open file only for reading
+	file = FileAccess::open(p_file, FileAccess::READ);
+	// error out if nothing opens
+	if (!file) {
+		return false;
+	}
+
+	// FFMPEG IO
+
+	// TODO: Buffer length? 3KB
+	const int buffer_size = 3 * 1024;
+	uint8_t *buffer = (uint8_t *)memalloc(buffer_size);
+	pIOCtx = avio_alloc_context(buffer, buffer_size, 0, file, ffmpeg_read_packet, nullptr, ffmpeg_seek_packet);
+
+	// FFMPEG format recognition
+	pFormatCtx = avformat_alloc_context();
+	pFormatCtx->pb = pIOCtx;
+
+	// Recognize the input format (or FFMPEG will kill the buffer by loading 5 MB : Should I try 5MB buffer?)
+	int read_bytes = file->get_buffer(buffer, buffer_size);
+	if (read_bytes < buffer_size) {
+		return false;
+	}
+
+	file->seek(0);
+	AVProbeData probe_data;
+	probe_data.buf = buffer;
+	probe_data.buf_size = read_bytes;
+	probe_data.filename = "";
+	pFormatCtx->iformat = av_probe_input_format(&probe_data, 1);
+
+	pFormatCtx->flags = AVFMT_FLAG_CUSTOM_IO;
+
+	if (avformat_open_input(&pFormatCtx, "", 0, 0) != 0) {
+		return false;
+	}
+
+	return true;
+}
+
+VideoStreamPlaybackFFMPEG::~VideoStreamPlaybackFFMPEG() {
+	avformat_close_input(&pFormatCtx);
+	av_free(pIOCtx);
+	if (file) {
+		memdelete(file);
+	}
 }
