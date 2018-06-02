aa71e77e09d533606111a5e822e105659d107a02
Anish <anishbhobe@hotmail.com>
2018-06-02 19:11:36 +0530

    minor edits

--- modules/gdnative/videodecoder/register_types.cpp
+++ modules/gdnative/videodecoder/register_types.cpp
@@ -1,9 +1,16 @@
 /* register_types.h */
 
 #include "class_db.h"
+#include "resource_importer_ffmpeg.h"
 #include "videostream_ffmpeg.h"
 
 void register_videodecoder_types() {
+
+#ifdef TOOLS_ENABLED
+	Ref<ResourceImporterFFMPEG> ffmpeg_import;
+	ffmpeg_import.instance();
+	ResourceFormatImporter::get_singleton()->add_importer(ffmpeg_import);
+#endif
 	ClassDB::register_class<VideoStreamFFMPEG>();
 }
 void unregister_videodecoder_types() {
